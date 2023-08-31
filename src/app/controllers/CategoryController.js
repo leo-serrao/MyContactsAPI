f870ae05474122f2bffc262922d849659b64facc
Leonardo Serrão <leoserraom@gmail.com>
2023-08-30 23:07:38 -0300

    created Categories CRUD (controller, repository and routes)

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,61 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(req, res) {
+    const categories = await CategoriesRepository.findAll();
+
+    res.json(categories);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return res.status(404).json({ error: '404 Category Not Found' });
+    }
+
+    res.json(category);
+  }
+
+  async store(req, res) {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    res.json(category);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return res.status(400).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    res.json(category);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await CategoriesRepository.delete(id);
+
+    res.sendStatus(204);
+  }
+}
+
+module.exports = new CategoryController();
